50461226c375ca9a8665114f1b53ebcebc91a0ec
Bryan Cora <corabj@mail.uc.edu>
2024-10-20 20:36:56 -0400

    leafnode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,10 +1,10 @@
 class HTMLNode:
     def __init__(
         self,
-        tag: str | None = None,
         value: str | None = None,
-        children: list["HTMLNode"] | None = None,
+        tag: str | None = None,
         props: dict[str, str] | None = None,
+        children: list["HTMLNode"] | None = None,
     ):
         self.tag = tag
         self.value = value
@@ -20,3 +20,22 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        value: str,
+        tag: str | None = None,
+        props: dict[str, str] | None = None,
+    ):
+        super().__init__(value, tag, props, None)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode must have value")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag} {self.props_to_html()}> {self.value}</{self.tag}>"
