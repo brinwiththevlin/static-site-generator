2a5adc4bb8f12d9acd408fcd2bf20b8aebee569a
Bryan Cora <corabj@mail.uc.edu>
2024-10-25 15:42:13 -0400

    "block to block type"

--- src/block.py
+++ src/block.py
@@ -1,3 +1,6 @@
+import re
+
+
 def markdown_to_blocks(markdown: str) -> list[str]:
     """takes a string that represents a whole block
 
@@ -12,3 +15,33 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     """
     blocks = markdown.split("\n\n")
     return list(map(lambda x: x.strip(), blocks))
+
+
+def block_to_block_type(block: str) -> str:
+    """takes a single markdown block and performs pattern mattching to return the type of block it is
+
+    starting with 1-6 # is a header
+    each line starting with > is a quote
+    each line starting with * or - is unordered list
+    \\d\\. at the start of every line is an ordered list
+    else just a paragraph
+
+    Args:
+        block: original block
+
+    Returns:
+        block type
+    """
+    pattern = re.compile(r"^#{1,6} ")
+    if re.match(pattern, block):
+        return "header"
+    lines = block.split("\n")
+    if all(map(lambda x: x[0] == ">", lines)):
+        return "quote"
+    if all(map(lambda x: x[0] == "*", lines)) or all(map(lambda x: x[0] == "-", lines)):
+        return "unordered list"
+
+    pattern = re.compile(r"^\s*\d\.")
+    if all(map(lambda x: re.match(pattern, x), lines)):
+        return "ordered list"
+    return "paragraph"
--- tests/test_block.py
+++ tests/test_block.py
@@ -1,9 +1,8 @@
 import unittest
 
-from block import markdown_to_blocks
+from block import block_to_block_type, markdown_to_blocks
 
 
-# markdown_to_blocks("")
 class TestBlock(unittest.TestCase):
     def test_one_block(self):
         markdown = "only one block of text"
@@ -13,3 +12,27 @@ class TestBlock(unittest.TestCase):
         markdown = "block one\n\nblock two\nhas two lines\n\nblock three"
         expected = ["block one", "block two\nhas two lines", "block three"]
         self.assertEqual(expected, markdown_to_blocks(markdown))
+
+    def test_paragraph(self):
+        markdown = "this is just a paragrapb\nnot special at all"
+        expected = "paragraph"
+        self.assertEqual(expected, block_to_block_type(markdown))
+
+    def test_quote(self):
+        markdown = ">quote\n>still quote\n>last quote"
+        expected = "quote"
+        self.assertEqual(expected, block_to_block_type(markdown))
+
+    def test_unordered(self):
+        markdown = "* this is\n* an unordered\n* list"
+        expected = "unordered list"
+        self.assertEqual(expected, block_to_block_type(markdown))
+
+    def test_ordered(self):
+        markdown = "1. this is\n2. an unordered\n3. list"
+        expected = "ordered list"
+        self.assertEqual(expected, block_to_block_type(markdown))
+
+
+if __name__ == "__main__":
+    unittest.main()
